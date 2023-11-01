59cacc9102e016c629d6d9f1f64cb9f1e521737f
Suhrb <suhrobjonmadaminov37@gmail.com>
2023-11-01 15:43:55 +0500

    F1 user class

---
+++ src/User.java
@@ -0,0 +1,35 @@
+public class User {
+    private String name;
+    private String age;
+    private String lastName;
+
+    public User(String name, String age, String lastName) {
+        this.name = name;
+        this.age = age;
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
